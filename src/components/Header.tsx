4246a415a95b8e91805cf236b8a5ae427c00e0a9
matmagamb <ceifa13@outlook.com>
2022-07-20 15:58:10 -0300

    teste2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ export function Header() {
         <header className="w-full h-[100vh] bg-transparent flex flex-1 items-center justify-center p-6">
             <div className="">
 
-                <h1 className="text-orange-400 text-4xl">Site, em construção</h1>
+                <h1 className="text-orange-400 text-4xl">Site em construção</h1>
             </div>
         </header>
     )
