a3f3be7d2e9f83a5e85eb32bd80839ac300a5160
matmagamb <ceifa13@outlook.com>
2022-07-20 14:24:31 -0300

    teste

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <header className="w-full h-[100vh] bg-transparent flex flex-1 items-center justify-center p-6">
+            <div className="">
+
+                <h1 className="text-orange-400 text-4xl">Site, em construção</h1>
+            </div>
+        </header>
+    )
+}
