12154355d22af4209120e2bad0f76eecc52b612d
matmagamb <ceifa13@outlook.com>
2022-07-29 12:56:55 -0300

    block 1 up

--- src/components/InicialBanner.tsx
+++ src/components/InicialBanner.tsx
@@ -1,12 +1,12 @@
 export function InicialBanner() {
     return (
         <section className=" flex flex-1 flex-col justify-center w-full h-[80vh] bg-banner bg-cover bg-right bg-origin-content bg-no-repeat">
-            <article className="pl-32" >
+            <article className="pl-40" >
 
                 <h1 className="text-4xl font-medium">consectetur adipiscing elit</h1>
                 <span className="text-3xl font-normal">nisi nec sodales pulvinar</span>
 
-                <div className="mt-12">
+                <div className="mt-16">
                     <a href="#" className="text-xl font-normal border rounded-full py-1 px-3">Quero ser mais saudavel</a>
                 </div>
 
