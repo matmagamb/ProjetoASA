ff5639db01b5661d29469fab11c328821ec23dd8
matmagamb <ceifa13@outlook.com>
2022-08-01 18:59:17 -0300

    block 3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { InicialBanner } from "../components/InicialBanner";
 import { SocialMidia } from "../components/SocialMidia";
 import Personal from "../assets/Personal.png";
 import Jump from "../assets/Jump.png";
+import Punch from "../assets/Punch.png";
 
 export function Home() {
     return (
@@ -16,10 +17,10 @@ export function Home() {
                     <img
                         src={Jump}
                         alt="mulher fazendo exercicio"
-                        className=" h-[70vh] border-4 border-dashed " />
+                        className=" h-[702px] min-w-[544px] border-4 border-dashed " />
                 </div>
 
-                <article className="max-w-[32vw] max-h-[70vh] flex flex-col justify-start ">
+                <article className="max-w-[500px] min-w-[500px] max-h-[700px] flex flex-col justify-start ">
 
                     <div className="pb-16 pt-36 " >
                         <img src={Personal} alt="texto Personal Trainer Online" />
@@ -30,6 +31,28 @@ export function Home() {
 
 
                     </p>
+                </article>
+
+            </section>
+
+            <section className="flex bg-[#E8E8E8]">
+                <div className="">
+                    <img src={Punch} alt="" className="" />
+                </div>
+                <article className="flex flex-1 justify-center items-center" >
+
+                    <div className="w-[500px] h-[500px] ">
+                        <div className="mb-8 mt-20">
+
+                            <h1 className="text-3xl font-bold">LOREN IPSUN</h1>
+                            <span className="text-2xl font-normal tracking-tighter leading-7 ">Personal trainer & Personal Fight</span>
+                        </div>
+                        <p className="text-md font-medium">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan, nisi nec sodales pulvinar, metus erat sollicitudin massa, placerat blandit lorem est imperdiet est. Proin ultrices magna vel mi commodo finibus. Nulla urna ex, imperdiet ut bibendum et, auctor sed mauris. Donec tempus a dolor in porttitor. Quisque ultrices, arcu in viverra vulputate, mi metus posuere mauris, non porttitor justo massa et mi. Donec dignissim nibh dui, et commodo risus lacinia ac. Lorem ipsum dolor sit amet,
+
+
+                        </p>
+                    </div>
                 </article>
 
             </section>
